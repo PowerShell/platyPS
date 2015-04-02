01dad929dae246d896dc65c02203c40e4333ab37
Sergei Vorobev <sevoroby@microsoft.com>
2015-04-01 18:46:06 -0700

    Add helper stuff

---
+++ setup.ps1
@@ -0,0 +1,6 @@
+﻿z maml
+$getProcessMaml = cat -RAW .\Get-Process.help.maml
+$maml = (cat -Raw C:\WINDOWS\system32\WindowsPowerShell\v1.0\en-US\Microsoft.PowerShell.Commands.Management.dll-help.xml)
+$xml = [xml]$maml
+$getProcessXml = ([xml]$getProcessMaml).command
+Import-Module -Force .\Transform.psm1
