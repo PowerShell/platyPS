afcd3bc2df3814806513f43d412bf80925f6193f
Sergei Vorobev <sevoroby@microsoft.com>
2015-07-27 15:01:29 -0700

    Add mock for renderer

---
+++ src/Markdown.MAML/Parser/MamlRenderer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Markdown.MAML.Model;
+
+namespace Markdown.MAML.Parser
+{
+    /// <summary>
+    /// This class contain logic to render maml from the Model.
+    /// </summary>
+    public class MamlRenderer
+    {
+        private StringBuilder _stringBuilder = new StringBuilder();
+
+        private const string XML_PREAMBULA =
+            @"<command:command xmlns:maml=""http://schemas.microsoft.com/maml/2004/10"" xmlns:command=""http://schemas.microsoft.com/maml/dev/command/2004/10"" xmlns:dev=""http://schemas.microsoft.com/maml/dev/2004/10"" xmlns:MSHelp=""http://msdn.microsoft.com/mshelp"">";
+
+        public MamlRenderer(DocumentNode root)
+        {
+            
+        }
+
+        public string ToMamlString()
+        {
+            _stringBuilder.Clear();
+            _stringBuilder.Append(XML_PREAMBULA);
+            return _stringBuilder.ToString();
+        }
+    }
+}
---
+++ test/Markdown.MAML.Test/Parser/MamlRendererTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Markdown.MAML.Model;
+using Markdown.MAML.Parser;
+using Xunit;
+
+namespace Markdown.MAML.Test.Parser
+{
+    public class MamlRendererTests
+    {
+        [Fact]
+        public void EmptyMarkdownProduceEmptyMaml()
+        {
+            var renderer = new MamlRenderer(new DocumentNode());
+            string maml = renderer.ToMamlString();
+            Assert.Equal(maml, @"<command:command xmlns:maml=""http://schemas.microsoft.com/maml/2004/10"" xmlns:command=""http://schemas.microsoft.com/maml/dev/command/2004/10"" xmlns:dev=""http://schemas.microsoft.com/maml/dev/2004/10"" xmlns:MSHelp=""http://msdn.microsoft.com/mshelp"">");
+        }
+    }
+}
