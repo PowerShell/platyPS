f4c7cf3dfacc69670ff4f80d3973c57bb5cd2260
David Wilson <daviwil@microsoft.com>
2015-07-28 12:06:02 -0700

    Redesign MarkdownParser to use data-driven model
    This change redesigns the MarkdownParser class to use a more data-driven
    approach to defining and acting upon Markdown regex patterns and their
    matches.  This allows developers to easily add new Markdown patterns
    without changing a lot of existing logic.

---
+++ src/Markdown.MAML/Parser/MarkdownPatternList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Markdown.MAML.Parser
+{
+    public class MarkdownPatternList : IEnumerable<MarkdownPattern>
+    {
+        private List<MarkdownPattern> _patternList = new List<MarkdownPattern>();
+        private Dictionary<string, Action<Match, Group>> _matchGroupActions = 
+            new Dictionary<string, Action<Match, Group>>();
+
+        public void Add(string groupName, string regexPattern, Action<Match, Group> matchAction)
+        {
+            _patternList.Add(
+                new MarkdownPattern(
+                    groupName,
+                    regexPattern,
+                    matchAction));
+
+
+            _matchGroupActions.Add(
+                groupName,
+                matchAction);
+        }
+
+        public bool TryExecuteMatchAction(string groupName, Match regexMatch, Group matchGroup)
+        {
+            Action<Match, Group> matchAction = null;
+
+            if (_matchGroupActions.TryGetValue(groupName, out matchAction))
+            {
+                matchAction(regexMatch, matchGroup);
+                return true;
+            }
+
+            return false;
+        }
+
+        public IEnumerator<MarkdownPattern> GetEnumerator()
+        {
+            return _patternList.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return _patternList.GetEnumerator();
+        }
+    }
+}
