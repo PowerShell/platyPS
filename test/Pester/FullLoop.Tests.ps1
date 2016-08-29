ec553d7dbd72ce408973f11231eeefc08e1a25e8
Yingda <v-yingdh@microsoft.com>
2016-08-29 18:49:10 +0800

    Update FullLoop.Tests.ps1

--- test/Pester/FullLoop.Tests.ps1
+++ test/Pester/FullLoop.Tests.ps1
@@ -90,10 +90,10 @@ Describe 'Full loop for Add-Member cmdlet' {
             $name = $genParam.name
             # By default is empty, set default value of 'False' if parametr is empty and type is 'SwitchParameter'
             # Otherwise, set default value of 'None' if parameter is empty
-            if([String]::IsNullOrEmpty($originalHelpObject.parameters.parameter[$_].defaultValue) -and $originalHelpObject.parameters.parameter[$_].type.name.Equals("SwitchParameter"))
-            {
-                $originalHelpObject.parameters.parameter[$_].defaultValue = "False"
-            }
+            #if([String]::IsNullOrEmpty($originalHelpObject.parameters.parameter[$_].defaultValue) -and $originalHelpObject.parameters.parameter[$_].type.name.Equals("SwitchParameter"))
+            #{
+            #    $originalHelpObject.parameters.parameter[$_].defaultValue = "False"
+            #}
             #elseif([String]::IsNullOrEmpty($originalHelpObject.parameters.parameter[$_].defaultValue))
             #{
             #    $originalHelpObject.parameters.parameter[$_].defaultValue = "None"
