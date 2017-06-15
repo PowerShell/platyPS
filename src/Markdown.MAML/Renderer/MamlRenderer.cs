413c84334e286459f7f3276331a158bda01f5bae
Mike Sampson <misampso@microsoft.com>
2017-06-15 10:41:25 -0700

    Responding to comments

--- src/Markdown.MAML/Renderer/MamlRenderer.cs
+++ src/Markdown.MAML/Renderer/MamlRenderer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using Markdown.MAML.Model.MAML;
 using System.Xml.Linq;
 
@@ -18,18 +19,13 @@ namespace Markdown.MAML.Renderer
         private static XNamespace devNS = XNamespace.Get("http://schemas.microsoft.com/maml/dev/2004/10");
         private static XNamespace msHelpNS = XNamespace.Get("http://msdn.microsoft.com/mshelp");
 
-        public string MamlModelToString(IEnumerable<MamlCommand> mamlCommands)
-        {
-            return MamlModelToString(mamlCommands, false);
-        }
-
         /// <summary>
         /// 
         /// </summary>
         /// <param name="mamlCommands"></param>
         /// <param name="skipPreambula">Don't include header and footer for xml doc</param>
         /// <returns></returns>
-        public string MamlModelToString(IEnumerable<MamlCommand> mamlCommands, bool skipPreambula)
+        public string MamlModelToString(IEnumerable<MamlCommand> mamlCommands)
         {
             var document = new XDocument(
                 new XDeclaration("1.0", "utf-8", null),
@@ -37,18 +33,34 @@ namespace Markdown.MAML.Renderer
                     new XAttribute("schema","maml"),
 
                     mamlCommands.Select(CreateCommandElement)));
-            using (var writer = new StringWriter())
+
+            var memoryStream = new MemoryStream();
+            using (var writer = new StreamWriter(memoryStream, Encoding.UTF8))
             {
                 document.Save(writer, SaveOptions.OmitDuplicateNamespaces);
-                return writer.ToString();
+                writer.Flush();
+
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                using (var reader = new StreamReader(memoryStream, Encoding.UTF8))
+                {
+                    return RenderCleaner.NormalizeWhitespaces(reader.ReadToEnd());
+                }
             }
         }
 
         private static XElement CreateCommandElement(MamlCommand command)
         {
             var commandParts = command.Name.Split('-');
-            var verb = commandParts[0];
-            var noun = commandParts[1];
+            string verb = null, noun;
+            if (commandParts.Length > 1)
+            {
+                verb = commandParts[0];
+                noun = commandParts[1];
+            }
+            else
+            {
+                noun = commandParts[0];
+            }
 
             return new XElement(commandNS + "command",
                     new XAttribute(XNamespace.Xmlns + "maml", mamlNS),
@@ -58,12 +70,12 @@ namespace Markdown.MAML.Renderer
 
                     new XElement(commandNS + "details",
                         new XElement(commandNS + "name", command.Name),
-                        new XElement(commandNS + "verb", verb),
+                        verb != null ? new XElement(commandNS + "verb", verb) : null,
                         new XElement(commandNS + "noun", noun),
                         new XElement(mamlNS + "description", GenerateParagraphs(command.Synopsis))),
                     new XElement(mamlNS + "description", GenerateParagraphs(command.Description)),
                     new XElement(commandNS + "syntax", command.Syntax.Select(syn => CreateSyntaxItem(syn, command))),
-                    new XElement(commandNS + "parameters", command.Parameters.Select(param => CreateParameter(param, false))),
+                    new XElement(commandNS + "parameters", command.Parameters.Select(param => CreateParameter(param))),
                     new XElement(commandNS + "inputTypes", command.Inputs.Select(input => CreateInput(input))),
                     new XElement(commandNS + "returnValues", command.Outputs.Select(output => CreateOutput(output))),
                     new XElement(mamlNS + "alertSet", 
@@ -87,10 +99,12 @@ namespace Markdown.MAML.Renderer
         {
             return new XElement(commandNS + "syntaxItem", 
                     new XElement(mamlNS + "name", command.Name),
-                    syntaxItem.Parameters.Select(param => CreateParameter(param, true)));
+                    syntaxItem.Parameters.Select(param => CreateParameter(param, isSyntax: true)));
         }
 
-        private static XElement CreateParameter(MamlParameter param, bool syntax)
+        // We pass a syntax flag due to a weired quirk inside <syntax>:
+        // we don't add [switch] info to make it appear good
+        private static XElement CreateParameter(MamlParameter param, bool isSyntax = false) 
         {
             string mamlType = ConvertPSTypeToMamlType(param);
             bool isSwitchParameter = mamlType == "SwitchParameter";
@@ -105,10 +119,10 @@ namespace Markdown.MAML.Renderer
 
                     new XElement(mamlNS + "name", param.Name),
                     new XElement(mamlNS + "Description", GenerateParagraphs(param.Description)),
-                    syntax && param.ParameterValueGroup.Any() 
+                    isSyntax && param.ParameterValueGroup.Any() 
                         ? new XElement(commandNS + "parameterValueGroup", param.ParameterValueGroup.Select(pvg => CreateParameterValueGroup(pvg))) 
                         : null,
-                    !syntax || !isSwitchParameter 
+                    !isSyntax || !isSwitchParameter 
                         ? new XElement(commandNS + "parameterValue",
                             new XAttribute("required", param.ValueRequired),
                             new XAttribute("variableLength", param.ValueVariableLength),
@@ -156,7 +170,11 @@ namespace Markdown.MAML.Renderer
 
         private static XElement CreateLink(MamlLink link)
         {
-            if(link.IsSimplifiedTextLink)
+            // PowerShell help engine is not happy, when LinkUri doesn't represent a valid URI
+            // but it's often convinient to have it (i.e. relative links between markdown files on github).
+            // so, we are ignoring non-uri links when rendering the final maml.
+            // https://github.com/PowerShell/platyPS/issues/164
+            if (link.IsSimplifiedTextLink)
             {
                 return null;
             }
@@ -177,7 +195,7 @@ namespace Markdown.MAML.Renderer
         {
             if (parameter.Type == null)
             {
-                return "";
+                return string.Empty;
             }
 
             if (parameter.IsSwitchParameter())
