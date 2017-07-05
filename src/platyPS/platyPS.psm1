9c29e39b38fdab6d48a17eba4ec5df12b6331da6
Sergei Vorobev <xvorsx@gmail.com>
2017-07-04 19:13:25 -0700

    Remove intermediate hashtable in applicable tags
    That always sort applicable tags alphabetically

--- src/platyPS/platyPS.psm1
+++ src/platyPS/platyPS.psm1
@@ -503,7 +503,7 @@ function Merge-MarkdownHelp
             $files = $_.Group
             $groupName = $_.Name
 
-            $dict = @{}
+            $dict = New-Object 'System.Collections.Generic.Dictionary[string, Markdown.MAML.Model.MAML.MamlCommand]'
             $files | ForEach-Object {
                 $model = GetMamlModelImpl $_.FullName -ForAnotherMarkdown -Encoding $Encoding
                 # unwrap List of 1 element
@@ -524,7 +524,7 @@ function Merge-MarkdownHelp
             }
 
             $merger = New-Object Markdown.MAML.Transformer.MamlMultiModelMerger -ArgumentList $null, !$ExplicitApplicableIfAll, $MergeMarker
-            $newModel = $merger.MergePS($dict)
+            $newModel = $merger.Merge($dict)
 
             $md = ConvertMamlModelToMarkdown -mamlCommand $newModel -metadata $newMetadata -PreserveFormatting
             $outputFilePath = Join-Path $OutputPath $groupName
