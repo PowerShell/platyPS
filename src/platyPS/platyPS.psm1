ee5c320d657e15c6334953650ea71cf9f3faf812
Thomas Nieto <38873752+tnieto88@users.noreply.github.com>
2018-10-14 14:04:50 -0500

    Add NewMarkdownParser Localized Data

--- src/platyPS/platyPS.psm1
+++ src/platyPS/platyPS.psm1
@@ -1568,7 +1568,7 @@ function NewMarkdownParser
     $warningCallback = GetWarningCallback
     $progressCallback = {
         param([int]$current, [int]$all)
-        Write-Progress -Activity "Parsing markdown" -status "Progress:" -percentcomplete ($current/$all*100)
+        Write-Progress -Activity $LocalizedData.ParsingMarkdown -status $LocalizedData.Progress -percentcomplete ($current/$all*100)
     }
     return new-object -TypeName 'Markdown.MAML.Parser.MarkdownParser' -ArgumentList ($progressCallback, $warningCallback)
 }
