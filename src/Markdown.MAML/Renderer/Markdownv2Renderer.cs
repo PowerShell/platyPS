e78798e76e6384e31bef065b1a188d9d53092a6d
PrzemyslawKlys <pklys@evotec.pl>
2019-01-22 13:05:46 +0100

    Fix fort Common and Workflow parameters autowrap

--- src/Markdown.MAML/Renderer/Markdownv2Renderer.cs
+++ src/Markdown.MAML/Renderer/Markdownv2Renderer.cs
@@ -73,18 +73,18 @@ namespace Markdown.MAML.Renderer
                     RenderCleaner.NormalizeQuotesAndDashes(
                         _stringBuilder.ToString())));
         }
-        
+
         private void AddYamlHeader(Hashtable yamlHeader)
         {
             _stringBuilder.AppendFormat("---{0}", NewLine);
-            
+
             // Use a sorted dictionary to force the metadata into alphabetical order by key for consistency.
             var sortedHeader = new SortedDictionary<string, string>(StringComparer.OrdinalIgnoreCase);
             foreach (DictionaryEntry pair in yamlHeader)
             {
                 sortedHeader[pair.Key.ToString()] = pair.Value == null ? "" : pair.Value.ToString();
             }
-            
+
             foreach (var pair in sortedHeader)
             {
                 AppendYamlKeyValue(pair.Key, pair.Value);
@@ -186,13 +186,13 @@ namespace Markdown.MAML.Renderer
         private void AddCommonParameters()
         {
             AddHeader(ModelTransformerBase.PARAMETERSET_NAME_HEADING_LEVEL, MarkdownStrings.CommonParametersToken, extraNewLine: false);
-            AddParagraphs(MarkdownStrings.CommonParametersText);
+            AddParagraphs(MarkdownStrings.CommonParametersText,false, true);
         }
 
         private void AddWorkflowParameters()
         {
             AddHeader(ModelTransformerBase.PARAMETERSET_NAME_HEADING_LEVEL, MarkdownStrings.WorkflowParametersToken, extraNewLine: false);
-            AddParagraphs(MarkdownStrings.WorkflowParametersText);
+            AddParagraphs(MarkdownStrings.WorkflowParametersText, false, true);
         }
 
         private Dictionary<string, MamlParameter> GetParamSetDictionary(string parameterName, List<MamlSyntax> syntaxes)
@@ -275,7 +275,7 @@ namespace Markdown.MAML.Renderer
             AddHeader(ModelTransformerBase.PARAMETERSET_NAME_HEADING_LEVEL, '-' + parameter.Name, extraNewLine: extraNewLine);
 
             AddParagraphs(parameter.Description);
-            
+
             var sets = SimplifyParamSets(GetParamSetDictionary(parameter.Name, command.Syntax));
             foreach (var set in sets)
             {
@@ -364,7 +364,7 @@ namespace Markdown.MAML.Renderer
         private static string GetExampleTitle(string title)
         {
             var match = Regex.Match(title, @"^(-| ){0,}(?<title>([^\f\n\r\t\v\x85\p{Z}-][^\f\n\r\t\v\x85]+[^\f\n\r\t\v\x85\p{Z}-]))(-| ){0,}$");
-            
+
             if (match.Success)
             {
                 return match.Groups["title"].Value;
@@ -387,7 +387,7 @@ namespace Markdown.MAML.Renderer
             sb.Append(command.Name);
 
             var paramStrings = new List<string>();
-                        
+
             // first we create list of param string we want to add
             foreach (var param in syntax.Parameters)
             {
@@ -411,7 +411,7 @@ namespace Markdown.MAML.Renderer
                         paramStr = string.Format("[{0}]", paramStr);
                     }
                 }
-                paramStrings.Add(paramStr);   
+                paramStrings.Add(paramStr);
             }
 
             if (command.IsWorkflow)
@@ -426,7 +426,7 @@ namespace Markdown.MAML.Renderer
 
             // then we format them properly with repsect to max width for window.
             int widthBeforeLastBreak = 0;
-            foreach (string paramStr in paramStrings) { 
+            foreach (string paramStr in paramStrings) {
 
                 if (sb.Length - widthBeforeLastBreak + paramStr.Length > maxSyntaxWidth)
                 {
@@ -495,7 +495,7 @@ namespace Markdown.MAML.Renderer
         private string GetAutoWrappingForMarkdown(string[] lines)
         {
             // this is an implementation of https://github.com/PowerShell/platyPS/issues/93
-            
+
             // algorithm: identify chunks that represent lists
             // Every entry in a list should be preserved as is and 1 EOL between them
             // Every entry not in a list should be split with GetAutoWrappingForNonListLine
@@ -534,14 +534,14 @@ namespace Markdown.MAML.Renderer
             return string.Join(NewLine, newLines);
         }
 
-        private void AddParagraphs(string body, bool noNewLines = false)
+        private void AddParagraphs(string body, bool noNewLines = false, bool skipAutoWrap = false)
         {
             if (string.IsNullOrWhiteSpace(body))
             {
                 return;
             }
 
-            if (this._mode != ParserMode.FormattingPreserve)
+            if (this._mode != ParserMode.FormattingPreserve && skipAutoWrap != true)
             {
                 string[] paragraphs = body.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
                 body = GetAutoWrappingForMarkdown(paragraphs.Select(para => GetEscapedMarkdownText(para.Trim())).ToArray());
@@ -581,7 +581,7 @@ namespace Markdown.MAML.Renderer
 
             // PS code: (((($text - replace '\\\\','\\\\') -replace '([<>])','\$1') -replace '\\([\[\]\(\)])', '\\$1')
 
-            // examples: 
+            // examples:
             // '\<' -> '\\\<'
             // '\' -> '\'
             // '\\' -> '\\\\'
