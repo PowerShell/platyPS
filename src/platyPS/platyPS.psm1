ec7e1af626149012e9fe0717a77fffd2afd03a4f
Scalia <MSCALIA@bethel.jw.org>
2019-03-15 14:16:19 -0400

    Modified GetSchemaVersion function to assume 2.0.0 if no schema version is found.

--- src/platyPS/platyPS.psm1
+++ src/platyPS/platyPS.psm1
@@ -1618,17 +1618,13 @@ function GetSchemaVersion
     if ($metadata)
     {
         $schema = $metadata[$script:SCHEMA_VERSION_YAML_HEADER]
-        if (-not $schema)
-        {
-            # there is metadata, but schema version is not specified.
-            # assume 2.0.0
-            $schema = '2.0.0'
-        }
     }
-    else
+
+    if (-not $schema)
     {
-        # if there is not metadata, then it's schema version 1.0.0
-        $schema = '1.0.0'
+        # either there is no metadata, or schema version is not specified.
+        # assume 2.0.0
+        $schema = '2.0.0'
     }
 
     return $schema
