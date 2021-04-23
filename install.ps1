9ee2637db9a7bcb42dd8df715af2c921c0a84bb5
Raiden Erdmann <raiden.erdmann@gmail.com>
2021-04-23 15:51:22 +0200

    more robust install

--- install.ps1
+++ install.ps1
@@ -2,8 +2,14 @@
 try {
     $destination = $env:PSModulePath -split ";" | Where-Object { $_ -like "*Program Files*" }
     if (-not $destination) { $destination = $env:PSModulePath -split ";" | Select-Object -First 1 }
-    Copy-Item "$PSScriptRoot\out\platyPS" -Destination $destination
+    Copy-Item "$PSScriptRoot\out\platyPS" -Destination $destination -Recurse
 }
 catch {
-    Write-Error "Could not install platyPS", $_
+    try {
+        Copy-Item "$PSScriptRoot\out\platyPS" -Destination ($env:PSModulePath -split ";" | Select-Object -First 1) -Recurse
+    }
+    catch {
+        Write-Error "Could not install platyPS"
+        throw $_
+    }
 }
