2e793bf582bdec3035a4aa3f38b21d69e9d9be1a
Raiden Erdmann <raiden.erdmann@gmail.com>
2021-04-23 15:51:54 +0200

    new stuff

---
+++ install.ps1
@@ -0,0 +1,17 @@
+.\build.ps1
+try {
+    $destination = $env:PSModulePath -split ";" | Where-Object { $_ -like "*Program Files*" }
+    if (-not $destination) { $destination = $env:PSModulePath -split ";" | Select-Object -First 1 }
+    Copy-Item "$PSScriptRoot\out\platyPS" -Destination $destination -Recurse
+}
+catch {
+    try {
+        Copy-Item "$PSScriptRoot\out\platyPS" -Destination ($env:PSModulePath -split ";" | Select-Object -First 1) -Recurse
+    }
+    catch {
+        Write-Error "Could not install platyPS"
+        throw $_
+    }
+}
+
+Import-Module platyPS # As a check if has been installed
