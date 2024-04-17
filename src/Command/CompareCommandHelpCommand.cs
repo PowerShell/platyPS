a6d4ea60d0a8c2346a8003e5912066bb40e95863
James Truher <jimtru@microsoft.com>
2024-04-17 13:38:20 -0700

    add compare command.

---
+++ src/Command/CompareCommandHelpCommand.cs
@@ -0,0 +1,149 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
+using System.Management.Automation;
+using Microsoft.PowerShell.PlatyPS.Model;
+using System.Linq;
+
+namespace Microsoft.PowerShell.PlatyPS
+{
+    /// <summary>
+    /// Cmdlet to import a markdown file and convert it to a CommandHelp object.
+    /// </summary>
+
+    [Cmdlet(VerbsData.Compare, "CommandHelp")]
+    public class CompareCommandHelpCommand : PSCmdlet
+    {
+        /// <summary>
+        /// The reference for comparison.
+        /// </summary>
+        [Parameter(Position = 0, Mandatory = true, ValueFromPipelineByPropertyName = true)]
+        public CommandHelp? Reference { get; set; }
+
+        [Parameter(Position = 1, Mandatory = true, ValueFromPipelineByPropertyName = true)]
+        public CommandHelp? Difference { get; set; }
+
+        [Parameter]
+        public string[] PropertyNamesToExclude { get; set; } = new string[0];
+
+        List<String> DiagnosticMessages = new();
+
+        protected override void ProcessRecord()
+        {
+            if (Reference is null)
+            {
+                WriteError(new ErrorRecord(new ArgumentNullException("Reference object may not be null"), "errorId", ErrorCategory.InvalidData, null));
+                return;
+            }
+
+            if (Difference is null)
+            {
+                WriteError(new ErrorRecord(new ArgumentNullException("Difference object may not be null"), "errorId", ErrorCategory.InvalidData, null));
+                return;
+            }
+
+
+            var refObject = new PSObject((CommandHelp)Reference);
+            var difObject = new PSObject((CommandHelp)Difference);
+
+            CompareProperty(refObject, difObject, 0);
+
+            WriteObject(DiagnosticMessages);
+            DiagnosticMessages.Clear();
+        }
+
+        private void CompareProperty(PSObject refObject, PSObject difObject, int offset)
+        {
+            var spaces = new string(' ', offset);
+            foreach (var property in refObject.Properties)
+            {
+                if (property.Value is String || ! (property.Value is IEnumerable))
+                {
+                    var difProperty = difObject.Properties.Where(p => string.Compare(property.Name, p.Name) == 0).First();
+                    if (LanguagePrimitives.Equals(difProperty.Value, property.Value))
+                    {
+                        var vString = property.Value is null ? string.Empty : property.Value.ToString();
+                        var val = vString.Length > 20 ? vString.Substring(0,17) : vString;
+                        DiagnosticMessages.Add($"{spaces}{property.Name} are the same ({val})");
+                    }
+                    else
+                    {
+                        DiagnosticMessages.Add($"{spaces}{property.Name} are not the same '{property.Value}' vs '{difProperty.Value}'");
+                    }
+                }
+                else if (property.Value is IDictionary)
+                {
+                    DiagnosticMessages.Add($"Inspecting {property.Name}");
+                    var rDictionary = (IDictionary)property.Value;
+                    var dDictionary = (IDictionary)difObject.Properties.Where(p => string.Compare(property.Name, p.Name) == 0).First().Value;
+                    foreach (var key in rDictionary.Keys)
+                    {
+                        if (LanguagePrimitives.Equals(rDictionary[key], dDictionary[key]))
+                        {
+                            DiagnosticMessages.Add($"{spaces}  {key} are the same ({rDictionary[key]})");
+                        }
+                        else
+                        {
+                            DiagnosticMessages.Add($"{spaces}  {key} not the same {rDictionary[key]} vs {dDictionary[key]}");
+                        }
+                    }
+
+                }
+                else if (property.Value is IList rList)
+                {
+                    DiagnosticMessages.Add($"Inspecting {property.Name}");
+                    var dList = (IList)difObject.Properties.Where(p => string.Compare(property.Name, p.Name) == 0).First().Value;
+                    if (rList.Count == 0 && dList.Count == 0)
+                    {
+                        DiagnosticMessages.Add($"{spaces}Lists are empty");
+                    }
+                    else if (rList.Count != dList.Count)
+                    {
+                        DiagnosticMessages.Add($"{spaces}Lists are different sizes ({rList.Count} vs {dList.Count})");
+                    }
+                    else
+                    {
+                        for (int i = 0; i < rList.Count; i++)
+                        {
+                            var rPSO = new PSObject(rList[i]);
+                            var dPSO = new PSObject(dList[i]);
+                            CompareProperty(rPSO, dPSO, offset + 2);
+                        }
+                    }
+                }
+                else if (property.Value is Array rArray)
+                {
+                    DiagnosticMessages.Add($"Inspecting {property.Name}");
+                    var dArray = (Array)difObject.Properties.Where(p => string.Compare(property.Name, p.Name) == 0).First().Value;
+                    if (rArray.Length == 0 && dArray.Length == 0)
+                    {
+                        DiagnosticMessages.Add($"{spaces}Arrays are empty");
+                    }
+                    else if (rArray.Length != dArray.Length)
+                    {
+                        DiagnosticMessages.Add($"{spaces}Arrays are different sizes ({rArray.Length} vs {dArray.Length})");
+                    }
+                    else
+                    {
+                        for (int i = 0; i < rArray.Length; i++)
+                        {
+                            var rPSO = new PSObject(rArray.GetValue(i));
+                            var dPSO = new PSObject(dArray.GetValue(i));
+                            CompareProperty(rPSO, dPSO, offset + 2);
+                        }
+                    }
+                }
+                else
+                {
+                    DiagnosticMessages.Add($"{spaces}Skipping {property.Name}");
+                }
+            }
+        }
+    }
+}
