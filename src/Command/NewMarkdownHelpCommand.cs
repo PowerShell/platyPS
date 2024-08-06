d5ca771c2dd613be8e5f473ee9c610ad9abd462a
James Truher <jimtru@microsoft.com>
2024-08-06 13:10:53 -0700

    use CurrentCulture rather than CurrentUICulture

--- src/Command/NewMarkdownHelpCommand.cs
+++ src/Command/NewMarkdownHelpCommand.cs
@@ -144,7 +144,7 @@ namespace Microsoft.PowerShell.PlatyPS
                         ExcludeDontShow = false,
                         FwLink = HelpInfoUri,
                         HelpVersion = HelpVersion.ToString(),
-                        Locale = Locale is null ? CultureInfo.CurrentUICulture : new CultureInfo(Locale),
+                        Locale = Locale is null ? CultureInfo.CurrentCulture : new CultureInfo(Locale),
                         ModuleName = cmd.ModuleName is null ? string.Empty : cmd.ModuleName,
                         ModuleGuid = cmd.Module?.Guid is null ? Guid.Empty : cmd.Module.Guid,
                         OnlineVersionUrl = HelpUri,
