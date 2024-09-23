711a3d710c69b8fb846babe2ab39f7a3cb0e8e44
James Truher <jimtru@microsoft.com>
2024-09-23 14:33:23 -0700

    Remove extraneous using statements.
    Fix bug in ProbeInfo enum for identifying file type.
    Add summaries for members.

--- src/MarkdownReader/MarkdownProbe.cs
+++ src/MarkdownReader/MarkdownProbe.cs
@@ -2,28 +2,19 @@
 // Licensed under the MIT License.
 
 using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using Markdig.Syntax;
-using Markdig.Syntax.Inlines;
-using Microsoft.PowerShell.PlatyPS.MarkdownWriter;
-using Microsoft.PowerShell.PlatyPS.Model;
-using System.CodeDom;
 
 namespace Microsoft.PowerShell.PlatyPS
 {
-
     /// <summary>
-    /// A class to represent a parse error.
-    /// This might occur at any time, but especially when invalid yaml is included in the markdown.
+    /// A class used for probing markdown to determine its characteristics.
     /// </summary>
     public class MarkdownProbe
     {
+        /// <summary>
+        /// Identify the type of platyps file based on the path.
+        /// </summary>
+        /// <param name="path">The path to a markdown file.</param>
+        /// <returns>MarkdownProbeInfo</returns>
         public static MarkdownProbeInfo Identify(string path)
         {
             return new MarkdownProbeInfo(path);
