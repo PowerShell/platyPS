33fc3868c28c269017258452e7ceee0a2c1fe081
Aditya Patwardhan <adityap@microsoft.com>
2025-03-07 11:07:49 -0800

    Ensure empty descriptions  notes, inputs, outputs and links create an accurate YAML (#720)

---
+++ test/Pester/YamlFormatting.Tests.ps1
@@ -0,0 +1,21 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+Describe 'Yaml formatting tests' {
+    It 'Inputs output and notes should have empty array when empty' {
+        Import-MarkdownCommandHelp -Path 'assets/New-EmptyCommand.md' | Export-YamlCommandHelp -OutputFolder $TestDrive
+        $yaml = Get-Content "$TestDrive/New-EmptyCommand.yml" -Raw
+
+        $isWinPS = $PSVersionTable.PSVersion.Major -eq 5
+
+        $inputPattern = if ($IsWindows -or $IsWinPS) { 'inputs:\r\n- name: System\.String\r\n  description: ''' } else { 'inputs:\n- name: System\.String\n  description: ''' }
+        $outputPattern = if ($IsWindows -or $IsWinPS) { 'outputs:\r\n- name: System\.String\r\n  description: ''' } else { 'outputs:\n- name: System\.String\n  description: ''' }
+        $notesPattern = "notes: ''"
+        $linksPattern = "links: \[\]"
+
+        $yaml | Should -Match $inputPattern
+        $yaml | Should -Match $outputPattern
+        $yaml | Should -Match $notesPattern
+        $yaml | Should -Match $linksPattern
+    }
+}
