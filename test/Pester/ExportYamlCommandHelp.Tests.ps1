c118a718661d42d870ce9eb44c6c7a8bb7ad19b7
Aditya Patwardhan <adityap@microsoft.com>
2025-07-02 14:37:03 -0700

    fix test file case

--- test/Pester/ExportYamlCommandHelp.Tests.ps1
+++ test/Pester/ExportYamlCommandHelp.Tests.ps1
@@ -165,7 +165,7 @@ Describe "Export-YamlCommandHelp tests" {
         }
 
         It 'Should be able to handle accepted values with NULL' {
-            $cmd = Import-MarkdownCommandHelp -Path "$PSScriptRoot/assets/Resolve-DNSName.md"
+            $cmd = Import-MarkdownCommandHelp -Path "$PSScriptRoot/assets/Resolve-DnsName.md"
             $yamlFile = $cmd | Export-YamlCommandHelp -outputfolder $TestDrive -Force
             $yamlDict = Import-YamlCommandHelp $yamlFile.FullName
             $yamlDict.Parameters[-1].AcceptedValues | Should -Contain "'NULL'"
