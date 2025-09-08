42e769a5b0c463921c9a981c75c93291eeecdd63
teramako <teramako@gmail.com>
2025-09-08 04:56:10 +0000

    Add `-Locale` parameter test for New-MarkdownCommandHelp

--- test/Pester/NewMarkdownHelp.Tests.ps1
+++ test/Pester/NewMarkdownHelp.Tests.ps1
@@ -657,4 +657,31 @@ Write-Host 'Hello World!'
             $file | Should -FileContentMatch 'Runs the command in a mode that only reports what would happen without performing the actions.'
         }
     }
+
+    Context 'Locale parameter' {
+        BeforeAll {
+            function global:Test-Locale {
+                [CmdletBinding()]
+                param()
+            }
+        }
+
+        It 'No Locale parameter' {
+            $file = New-MarkdownCommandHelp -Command (Get-Command 'Test-Locale') -OutputFolder "$TestDrive/NewMarkdownHelp" -Force
+
+            Get-Content -Path $file -First 10 | Where-Object { $_ -match "^Locale" } | Should -Be "Locale: en-US"
+        }
+
+        It 'Invariant Locale parameter' {
+            $file = New-MarkdownCommandHelp -Command (Get-Command 'Test-Locale') -OutputFolder "$TestDrive/NewMarkdownHelp" -Force -Locale C
+
+            Get-Content -Path $file -First 10 | Where-Object { $_ -match "^Locale" } | Should -Be "Locale: en-US"
+        }
+
+        It 'ja-JP Locale parameter' {
+            $file = New-MarkdownCommandHelp -Command (Get-Command 'Test-Locale') -OutputFolder "$TestDrive/NewMarkdownHelp" -Force -Locale ja-JP
+
+            Get-Content -Path $file -First 10 | Where-Object { $_ -match "^Locale" } | Should -Be "Locale: ja-JP"
+        }
+    }
 }
